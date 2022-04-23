ced70d7e03c70d0425c2e2cdf693082e307ff8e5
mahfuj10 <mahfujglobal@gmail.com>
2022-04-23 17:32:27 +0600

    aded listen function

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ io.on("connection", (socket) => {
 app.get('/', (req, res) => {
     res.send("Server runnint")
 })
-server.listen(5000, () => {
-    console.log("server running");
-})
+app.listen(process.env.PORT || 5000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
